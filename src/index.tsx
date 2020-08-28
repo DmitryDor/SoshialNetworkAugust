b2599dd561e9d71e3b50bfdf21e3439dab25f414
DmitryDor <dmitrydoroshenko80@gmail.com>
2020-08-28 16:37:38 +0300

    lesson38-39(diapatch, action, action creater)

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import { store} from "./redux/state";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
+export let rerenderEntireTree = () => {
+
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={store.getState()} dispatch={store.dispatch.bind(store)} />
+        </BrowserRouter>, document.getElementById('root')
+    );
+}
+
+  rerenderEntireTree();
+
+
+store.subscribe(rerenderEntireTree);
+
 serviceWorker.unregister();
