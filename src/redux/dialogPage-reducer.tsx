e76e919fe11ba5d46f7191c2ca1c567a907e5ba6
DmitryDor <dmitrydoroshenko80@gmail.com>
2020-09-15 22:19:20 +0300

    lesson41( reducer)

---
+++ src/redux/dialogPage-reducer.tsx
@@ -0,0 +1,55 @@
+import {AddPostActionType, MessagesType, UpdateNewPostTextType} from "./state";
+
+let initialeState = {
+
+        dialogs: [
+            {id: "1", name: "Dimych"},
+            {id: "2", name: "Andrey"},
+            {id: "3", name: "Sveta"},
+            {id: "4", name: "Sasha"},
+            {id: "5", name: "Victor"},
+            {id: "6", name: "Valera"}
+        ],
+        messages: [
+            {id: "1", message: "Hi"},
+            {id: "2", message: "How are you?"},
+            {id: "3", message: "I'm fine"},
+            {id: "4", message: "I'm happy"},
+            {id: "5", message: "Happy new year"}
+        ],
+        newMessageText: ''
+}
+
+export type AddMessageActionType = {
+    type: 'ADD-MESSAGE'
+}
+export type UpdateNewMessageTextType = {
+    type: 'UPDATE-NEW-MESSAGE-TEXT'
+    newMessage: string
+}
+
+export type ActionsType =  AddPostActionType | UpdateNewPostTextType | AddMessageActionType | UpdateNewMessageTextType
+
+
+export type DialogsStateType = typeof initialeState
+
+
+export const dialogsPageReducer = (state: DialogsStateType, action: ActionsType) => {
+    switch (action.type){
+        case "ADD-MESSAGE":
+            let newMessage: MessagesType = {id: "6", message: state.newMessageText};
+            state.messages.push(newMessage)
+            state.newMessageText = '';
+            return state
+        case "UPDATE-NEW-MESSAGE-TEXT":
+            state.newMessageText = action.newMessage
+            return state
+        default:
+            return state
+    }
+}
+
+export const addMessageAC = (): AddMessageActionType => ({type: 'ADD-MESSAGE'})
+
+export const onMessageChangeAC = (newMessage: string): UpdateNewMessageTextType =>
+    ({ type: 'UPDATE-NEW-MESSAGE-TEXT', newMessage: newMessage })
---
+++ src/redux/profilePage-reducer.tsx
@@ -0,0 +1,57 @@
+import {AddMessageActionType, UpdateNewMessageTextType} from "./state";
+
+export type AddPostActionType = {
+    type: 'ADD-POST'
+}
+export type UpdateNewPostTextType = {
+    type: 'UPDATE-NEW-POST-TEXT'
+    newPost: string
+}
+export type PostType = {
+    id: string,
+    message: string,
+    likesCount: number
+}
+
+let initialeState = {
+
+    posts: [
+        {id: "1", message: "Hellooo", likesCount: 10},
+        {id: "2", message: "Hi", likesCount: 5},
+        {id: "3", message: "Fine", likesCount: 7},
+        {id: "4", message: "World", likesCount: 8},
+        {id: "5", message: "New year", likesCount: 5},
+        {id: "6", message: "Bah", likesCount: 18}
+    ],
+    newPostText: ''
+
+
+}
+export type ActionsType = AddPostActionType | UpdateNewPostTextType | AddMessageActionType | UpdateNewMessageTextType
+
+
+export type ProfileStateType = typeof initialeState
+
+
+export const profilePageReducer = (state: ProfileStateType = initialeState, action: ActionsType) => {
+    switch (action.type){
+        case "ADD-POST":
+            let newPost: PostType = {id: '5', message: state.newPostText, likesCount: 0};
+            state.posts.push(newPost);
+            state.newPostText = '';
+            return state
+        case "UPDATE-NEW-POST-TEXT":
+            if(action.newPost){
+                state.newPostText = action.newPost
+            }
+            return state;
+        default:
+            return state
+    }
+
+}
+
+export const addPostAC = (): AddPostActionType => ({type: 'ADD-POST'})
+
+export const onPostChangeAC = (newText: string): UpdateNewPostTextType =>
+    ({ type: 'UPDATE-NEW-POST-TEXT', newPost: newText })
